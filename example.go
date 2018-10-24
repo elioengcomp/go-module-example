af3632014e0ea86cea3da814bc076942a9f5ef6e
Elio Marcolino <eliom@jfrog.com>
2018-10-24 10:45:36 -0700

    Version 2.0.0 not converted

--- example.go
+++ example.go
@@ -1,5 +1,7 @@
 package example
 
+const version = "v2.0.0"
+
 func Exec() string {
-	return "This is go-module-example project!"
+	return "This is go-module-example project " + version
 }
--- example_test.go
+++ example_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestExample(t *testing.T) {
 	result := Exec()
-	if result != "This is go-module-example project!" {
+	if result != "This is go-module-example project "+version {
 		t.Errorf("Failed to test example package")
 	}
 }
